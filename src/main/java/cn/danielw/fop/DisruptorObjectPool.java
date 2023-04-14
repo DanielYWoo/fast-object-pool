2bfdf5c696936ffa1adc53342604fa46ab26b31d
Daniel Wu <daniel.y.woo@gmail.com>
2023-04-14 22:01:00 +0800

    Update DisruptorObjectPool.java

--- src/main/java/cn/danielw/fop/DisruptorObjectPool.java
+++ src/main/java/cn/danielw/fop/DisruptorObjectPool.java
@@ -4,6 +4,9 @@ import com.conversantmedia.util.concurrent.DisruptorBlockingQueue;
 
 import java.util.concurrent.BlockingQueue;
 
+/**
+* This pool has disruptor as the underlying blocking queue.
+*/
 public class DisruptorObjectPool<T> extends ObjectPool<T> {
 
     public DisruptorObjectPool(PoolConfig poolConfig, ObjectFactory<T> objectFactory) {
